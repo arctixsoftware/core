15deac1562eb8e7d1d464ef795c0379927b5a166
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-29 15:44:18 +0300

    Create Factory.php

---
+++ src/Factories/Factory.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Core\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory as LaravelParentFactory;
+
+/**
+ * Factory
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class Factory extends LaravelParentFactory
+{
+
+}
