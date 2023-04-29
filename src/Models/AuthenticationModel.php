195b28ca8e1ef1a45dddd59bf30a8b13b229accb
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-29 23:29:06 +0300

    Create AuthenticationModel.php

---
+++ src/Models/AuthenticationModel.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Core\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+
+/**
+ * AuthenticationModel
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AuthenticationModel extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
+}
